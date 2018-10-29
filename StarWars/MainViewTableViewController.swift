93cc5201e15fabc46fdc69f2f44ab1396c5088b7
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-10-29 10:23:30 -0400

    Started to add table views function

--- StarWars/MainViewTableViewController.swift
+++ StarWars/MainViewTableViewController.swift
@@ -15,5 +15,12 @@ class MainViewTableViewController: UITableViewController {
 
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
