91f1d4b850e79ad081fd340220e3042b898cb579
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-10-29 10:15:32 -0400

    Initial commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Mitchell Socia on 10/29/18.
+//  Copyright © 2018 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
