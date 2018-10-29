5d1075fe4daaa15d1272bad2264864577cb80e89
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-10-29 16:20:13 -0400

    very stuck

--- StarWars/MainViewTableViewController.swift
+++ StarWars/MainViewTableViewController.swift
@@ -19,18 +19,17 @@ class MainViewTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         empire = networkCall.parse(data: networkCall.performStoreRequest(with: networkCall.empireURL())!)
-//        empire?.characters.first
+        //        empire?.characters.first
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return characters.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
-//        let characterName = characterDetails[indexPath.row]
         cell.textLabel?.text = empire?.characters[indexPath.row]
         return cell
     }
-
+    
 }
