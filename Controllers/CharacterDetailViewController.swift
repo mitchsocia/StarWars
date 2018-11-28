965e8c8645848404775003fe9cbd03ae9462c29f
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-11-28 09:54:25 -0500

    WIP Adding functions

--- Controllers/CharacterDetailViewController.swift
+++ Controllers/CharacterDetailViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class CharacterDetailViewController: UIViewController {
-
+    
     var person: Person?
     var homeWorld: Homeworld?
+    
+    @IBOutlet weak var characterNameLabel: UILabel!
+    @IBOutlet weak var characterBirthYearLabel: UILabel!
+    @IBOutlet weak var characterGenderLabel: UILabel!
+    @IBOutlet weak var characterHomeWorldLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +26,32 @@ class CharacterDetailViewController: UIViewController {
         characterHomeWorldLabel.text = person?.homeworld
         
     }
-
-    @IBOutlet weak var characterNameLabel: UILabel!
-    @IBOutlet weak var characterBirthYearLabel: UILabel!
-    @IBOutlet weak var characterGenderLabel: UILabel!
-    @IBOutlet weak var characterHomeWorldLabel: UILabel!
+    
+    func getHomeworldURL(from homeworldString: String) -> URL? {
+        guard let url = URL(string: homeworldString) else { return nil }
+        
+        return url
+    }
+    
+    func getHomeworldData(from url: URL) {
+        let defaultSession = URLSession(configuration: .default)
+        let urlRequest = URLRequest(url: url)
+        let dataTask = defaultSession.dataTask(with: urlRequest) { (data, urlResponse, error) in
+            
+            guard let data = data else {
+                return
+            }
+            
+            let currentPerson = self.parse(data: data)
+            self.characters.append(currentPerson!)
+            if self.characters.count == self.characterURLs.count {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        dataTask.resume()
+    }
     
     
 }
